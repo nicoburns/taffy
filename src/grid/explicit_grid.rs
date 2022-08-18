bb8ee57207736c61696e158626a2e77382410786
Nico Burns <nico@nicoburns.com>
2022-08-18 14:13:45 +0100

    Refactor main grid compute function

--- src/grid/explicit_grid.rs
+++ src/grid/explicit_grid.rs
@@ -1,4 +1,5 @@
-use super::types::{GridAxisTracks, GridTrack};
+use super::placement::TrackCounts;
+use super::types::GridTrack;
 use crate::style::{Dimension, FlexboxLayout, TrackSizingFunction};
 use crate::sys::GridTrackVec;
 use core::cmp::max;
@@ -10,38 +11,60 @@ pub(crate) fn compute_explicit_grid_size(style: &FlexboxLayout) -> (u16, u16) {
     (explicit_col_count, explicit_row_count)
 }
 
-/// 7.1. The Explicit Grid
-/// Initialise the `rows` and `columns` fields of the `Grid` based on following style properties:
-/// - `grid-template-rows`
-/// - `grid-template-columns`
-pub(in crate::grid) fn resolve_explicit_grid_tracks(
-    axis: &mut GridAxisTracks,
+pub(in crate::grid) fn initialize_grid_tracks(
+    tracks: &mut Vec<GridTrack>,
+    counts: TrackCounts,
     track_template: &GridTrackVec<TrackSizingFunction>,
+    auto_tracks: &Vec<TrackSizingFunction>,
     gap: Dimension,
 ) {
-    debug_assert!(
-        axis.len() == axis.origin,
-        "Number of populated tracks should be equal to origin when calling resolve_explicit_grid_track"
-    );
+    // Clear vector (in case this is a re-layout), reserve space for all tracks ahead of time to reduce allocations,
+    // and push the initial gutter
+    tracks.clear();
+    tracks.reserve((counts.len() * 2) + 1);
+    tracks.push(GridTrack::gutter(gap));
 
-    let mut track_count = 0;
-    track_template.iter().enumerate().for_each(|(index, track_sizing_function): (usize, &TrackSizingFunction)| {
-        // Generate gutter in between each track
-        if index != 0 {
-            axis.push(GridTrack::gutter(gap))
-        }
+    // Create negative implicit tracks
+    if auto_tracks.is_empty() {
+        let iter = core::iter::repeat(TrackSizingFunction::Auto);
+        create_implicit_tracks(tracks, counts.negative_implicit, iter, gap)
+    } else {
+        let offset = auto_tracks.len() % counts.negative_implicit as usize;
+        let iter = auto_tracks.iter().copied().skip(offset).cycle();
+        create_implicit_tracks(tracks, counts.negative_implicit, iter, gap)
+    }
 
-        // Generate track
-        axis.push(GridTrack::new(
+    // Create explicit tracks
+    track_template.iter().for_each(|track_sizing_function| {
+        tracks.push(GridTrack::new(
             track_sizing_function.min_sizing_function(),
             track_sizing_function.max_sizing_function(),
         ));
-
-        // Count track
-        track_count += 1;
+        tracks.push(GridTrack::gutter(gap))
     });
 
-    axis.explicit_track_count = track_count;
+    // Create positive implicit tracks
+    if auto_tracks.is_empty() {
+        let iter = core::iter::repeat(TrackSizingFunction::Auto);
+        create_implicit_tracks(tracks, counts.negative_implicit, iter, gap)
+    } else {
+        let iter = auto_tracks.iter().copied().cycle();
+        create_implicit_tracks(tracks, counts.negative_implicit, iter, gap)
+    }
+}
+
+// Utility function for repeating logic of creating implicit tracks
+fn create_implicit_tracks(
+    tracks: &mut Vec<GridTrack>,
+    count: u16,
+    mut auto_tracks_iter: impl Iterator<Item = TrackSizingFunction>,
+    gap: Dimension,
+) {
+    for _ in 0..count {
+        let track_def = auto_tracks_iter.next().unwrap();
+        tracks.push(GridTrack::new(track_def.min_sizing_function(), track_def.max_sizing_function()));
+        tracks.push(GridTrack::gutter(gap));
+    }
 }
 
 #[cfg(test)]

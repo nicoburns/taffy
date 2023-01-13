c24d4eba0834092b4310bdba4198bc76eb2f32af
Nico Burns <nico@nicoburns.com>
2023-01-13 15:45:32 +0000

    Reorder LayoutTree methods into current node and child node sections

--- src/tree.rs
+++ src/tree.rs
@@ -12,13 +12,35 @@ use crate::{
 /// Generally, Taffy expects your Node tree to be indexable by stable indices. A "stable" index means that the Node's ID
 /// remains the same between re-layouts.
 pub trait LayoutTree {
+    // Current node methods
+
+    /// Get the ID for the given node
+    fn current_node_id(&self) -> Node;
+
+    /// Get the [`Style`] for this Node.
+    fn style(&self) -> &Style;
+
+    /// Get the node's output "Final Layout"
+    fn layout(&self) -> &Layout;
+
+    /// Modify the node's output layout
+    fn layout_mut(&mut self) -> &mut Layout;
+
+    /// Get a cache entry for this Node by index
+    fn cache_mut(&mut self, index: usize) -> &mut Option<Cache>;
+
+    /// Measure a node. Taffy uses this to force reflows of things like text and overflowing content.
+    fn measure_node(&self, known_dimensions: Size<Option<f32>>, available_space: Size<AvailableSpace>) -> Size<f32>;
+
+    /// Node needs to be measured
+    fn needs_measure(&self) -> bool;
+
+    // Child methods
+
     /// Type representing an iterator of the children of a node
     type ChildIter<'a>: Iterator<Item = &'a DefaultKey>
     where
         Self: 'a;
-
-    /// Get the ID for the given node
-    fn current_node_id(&self) -> Node;
 
     /// Get the list of children IDs for the given node
     fn children(&self) -> Self::ChildIter<'_>;
@@ -32,18 +54,6 @@ pub trait LayoutTree {
     /// Get a specific child of a node, where the index represents the nth child
     fn child(&self, index: usize) -> Node;
 
-    // todo: allow abstractions over this so we don't prescribe how layout works
-    // for reference, CSS cascades require context, and storing a full flexbox layout for each node could be inefficient
-    //
-    /// Get the [`Style`] for this Node.
-    fn style(&self) -> &Style;
-
-    /// Get the node's output "Final Layout"
-    fn layout(&self) -> &Layout;
-
-    /// Modify the node's output layout
-    fn layout_mut(&mut self) -> &mut Layout;
-
     /// Get the [`Style`] for this child.
     fn child_style(&self, child_node_id: Node) -> &Style;
 
@@ -52,15 +62,6 @@ pub trait LayoutTree {
 
     /// Modify the child's output layout
     fn child_layout_mut(&mut self, child_node_id: Node) -> &mut Layout;
-
-    /// Measure a node. Taffy uses this to force reflows of things like text and overflowing content.
-    fn measure_node(&self, known_dimensions: Size<Option<f32>>, available_space: Size<AvailableSpace>) -> Size<f32>;
-
-    /// Node needs to be measured
-    fn needs_measure(&self) -> bool;
-
-    /// Get a cache entry for this Node by index
-    fn cache_mut(&mut self, index: usize) -> &mut Option<Cache>;
 
     /// Compute the size of the node given the specified constraints
     fn measure_child_size(

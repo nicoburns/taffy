28bd688b8654033c68df8c9c646e9f62f3768aab
Nico Burns <nico@nicoburns.com>
2023-04-14 23:41:38 +0100

    Add SyncMeasureFunc type

--- src/tree/measure_func.rs
+++ src/tree/measure_func.rs
@@ -8,8 +8,17 @@ use crate::util::sys::Box;
 /// A function type that can be used in a [`MeasureFunc`]
 ///
 /// This trait is automatically implemented for all types (including closures) that define a function with the appropriate type signature.
-pub trait Measurable: Send + Sync + Fn(Size<Option<f32>>, Size<AvailableSpace>) -> Size<f32> {}
-impl<F: Send + Sync + Fn(Size<Option<f32>>, Size<AvailableSpace>) -> Size<f32>> Measurable for F {}
+pub trait Measurable {
+    /// Measure node
+    fn measure(&self, known_dimensions: Size<Option<f32>>, available_space: Size<AvailableSpace>) -> Size<f32>;
+}
+pub trait SyncMeasurable: Send + Sync + Measurable {}
+impl<F: Fn(Size<Option<f32>>, Size<AvailableSpace>) -> Size<f32>> Measurable for F {
+    fn measure(&self, known_dimensions: Size<Option<f32>>, available_space: Size<AvailableSpace>) -> Size<f32> {
+        self(known_dimensions, available_space)
+    }
+}
+impl<F: Send + Sync + Fn(Size<Option<f32>>, Size<AvailableSpace>) -> Size<f32>> SyncMeasurable for F {}
 
 /// A function that can be used to compute the intrinsic size of a node
 pub enum MeasureFunc {
@@ -21,14 +30,43 @@ pub enum MeasureFunc {
     Boxed(Box<dyn Measurable>),
 }
 
-impl MeasureFunc {
+impl Measurable for MeasureFunc {
     /// Call the measure function to measure to the node
     #[inline(always)]
-    pub fn measure(&self, known_dimensions: Size<Option<f32>>, available_space: Size<AvailableSpace>) -> Size<f32> {
+    fn measure(&self, known_dimensions: Size<Option<f32>>, available_space: Size<AvailableSpace>) -> Size<f32> {
         match self {
-            MeasureFunc::Raw(measure) => measure(known_dimensions, available_space),
+            Self::Raw(measure) => measure(known_dimensions, available_space),
             #[cfg(any(feature = "std", feature = "alloc"))]
-            MeasureFunc::Boxed(measure) => (measure as &dyn Fn(_, _) -> _)(known_dimensions, available_space),
+            Self::Boxed(measurable) => measurable.measure(known_dimensions, available_space),
         }
     }
+}
+
+/// A function that can be used to compute the intrinsic size of a node
+pub enum SyncMeasureFunc {
+    /// Stores an unboxed function
+    Raw(fn(Size<Option<f32>>, Size<AvailableSpace>) -> Size<f32>),
+
+    /// Stores a boxed function
+    #[cfg(any(feature = "std", feature = "alloc"))]
+    Boxed(Box<dyn SyncMeasurable>),
+}
+
+impl Measurable for SyncMeasureFunc {
+    /// Call the measure function to measure to the node
+    #[inline(always)]
+    fn measure(&self, known_dimensions: Size<Option<f32>>, available_space: Size<AvailableSpace>) -> Size<f32> {
+        match self {
+            Self::Raw(measure) => measure(known_dimensions, available_space),
+            #[cfg(any(feature = "std", feature = "alloc"))]
+            Self::Boxed(measurable) => measurable.measure(known_dimensions, available_space),
+        }
+    }
+}
+
+
+#[test]
+fn ensure_sync_measure_func_is_send_sync() {
+    fn assert_send_sync<T: Send + Sync>() {}
+    assert_send_sync::<SyncMeasureFunc>()
 }

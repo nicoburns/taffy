62a72cea43df3ba1c0c624acf1e18b26707aba70
Nico Burns <nico@nicoburns.com>
2023-04-23 16:56:15 +0100

    Update test categories WIP

--- scripts/export-fixtures-to-yoga.sh
+++ scripts/export-fixtures-to-yoga.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# List yoga fixture ids (should be run in fixtures dir in yoga repo)
+# fd ".html" -j 1 -x /bin/bash -c 'echo {} && grep -oE '"'"'id="[^"]*"'"'"' {} | sd "id=\"(.*)\"" '"'"'$1'"'"' && echo ""'
+
 # Create versions of the fixtures with:
 #   - The template wrapper stripped
 #   - The "test-root" id replaced with the name of the test
@@ -20,8 +23,14 @@ for file_name in $DISABLED_FIXTURES; do
   mv yoga_test_fixtures/$file_name yoga_test_fixtures/$new_file_name
 done
 
+# Delete measure fixtures as yoga's test generator doesn't support this yet
+(cd yoga_test_fixtures && rm measure*.html)
+
 # Delete CSS Grid fixtures as yoga doesn't support CSS Grid yet
 (cd yoga_test_fixtures && rm grid*.html)
+
+# Delete percentage gap fixtures as yoga doesn't support percentage gaps yet
+(cd yoga_test_fixtures && fd "gap(.*)percent(.*).html" -x rm {})
 
 # Delete rounding fixtures as rounding may differ between taffy and yoga
 # (cd yoga_test_fixtures && rm rounding*.html)
@@ -32,6 +41,7 @@ rm yoga_test_fixtures/margin_left.html yoga_test_fixtures/margin_right.html
 YGAbsolutePositionTests=(
   absolute_layout_width_height_start_top
   absolute_layout_width_height_end_bottom
+  absolute_layout_row_width_height_end_bottom
   absolute_layout_start_top_end_bottom
   absolute_layout_width_height_start_top_end_bottom
   do_not_clamp_height_of_absolute_node_to_height_of_its_overflow_hidden_parent
@@ -51,9 +61,11 @@ YGAbsolutePositionTests=(
   absolute_layout_in_wrap_reverse_row_container
   absolute_layout_in_wrap_reverse_column_container_flex_end
   absolute_layout_in_wrap_reverse_row_container_flex_end
-  percent_absolute_position_infinite_height
-  absolute_layout_percentage_height_based_on_padded_parent
-  absolute_layout_percentage_height_based_on_padded_parent_and_align_items_center
+  absolute_layout_percentage_height
+  absolute_child_with_cross_margin
+  absolute_child_with_main_margin
+  absolute_child_with_max_height
+  absolute_child_with_max_height_larger_shrinkable_grandchild
 )
 
 
@@ -77,10 +89,20 @@ YGAlignContentTests=(
   align_content_stretch_row_with_min_height
   align_content_stretch_column
   align_content_stretch_is_not_overriding_align_items
+
+  # New from Taffy
+  align_content_not_stretch_with_align_items_stretch
+  align_content_space_around_single_line
+  align_content_space_around_wrapped
+  align_content_space_between_single_line
+  align_content_space_between_wrapped
+  align_content_space_evenly_single_line
+  align_content_space_evenly_wrapped
 )
 
 YGAlignItemsTests=(
   align_items_stretch
+  align_items_stretch_min_cross
   align_items_center
   align_items_flex_start
   align_items_flex_end
@@ -108,10 +130,20 @@ YGAlignItemsTests=(
   align_flex_start_with_shrinking_children
   align_flex_start_with_stretching_children
   align_flex_start_with_shrinking_children_with_stretch
+  align_items_center_justify_content_center
+  align_baseline_child_margin_percent
+  align_baseline_nested_column
+
+  # New from Taffy
+  align_baseline_nested_child
+  align_items_center_min_max_with_padding
+  align_items_center_with_child_margin
+  align_items_center_with_child_top
 )
 
 YGAlignSelfTests=(
   align_self_center
+  align_self_center_undefined_max_height
   align_self_flex_end
   align_self_flex_start
   align_self_flex_end_override_flex_start
@@ -122,12 +154,56 @@ YGAndroidNewsFeedTests=(
   android_news_feed
 )
 
+# New from Taffy
+YGAspectRatioTests=(
+  aspect_ratio_flex_column_fill_height
+  aspect_ratio_flex_column_fill_max_height
+  aspect_ratio_flex_column_fill_max_width
+  aspect_ratio_flex_column_fill_min_height
+  aspect_ratio_flex_column_fill_min_width
+  aspect_ratio_flex_column_fill_width
+  aspect_ratio_flex_column_fill_width_flex
+  aspect_ratio_flex_column_stretch_fill_height
+  aspect_ratio_flex_column_stretch_fill_max_height
+  aspect_ratio_flex_column_stretch_fill_max_width
+  aspect_ratio_flex_column_stretch_fill_min_height
+  aspect_ratio_flex_column_stretch_fill_min_width
+  aspect_ratio_flex_column_stretch_fill_width
+  aspect_ratio_flex_row_fill_height
+  aspect_ratio_flex_row_fill_max_height
+  aspect_ratio_flex_row_fill_max_width
+  aspect_ratio_flex_row_fill_min_height
+  aspect_ratio_flex_row_fill_min_width
+  aspect_ratio_flex_row_fill_width
+  aspect_ratio_flex_row_fill_width_flex
+  aspect_ratio_flex_row_stretch_fill_height
+  aspect_ratio_flex_row_stretch_fill_max_height
+  aspect_ratio_flex_row_stretch_fill_max_width
+  aspect_ratio_flex_row_stretch_fill_min_height
+  aspect_ratio_flex_row_stretch_fill_min_width
+  aspect_ratio_flex_row_stretch_fill_width
+  absolute_aspect_ratio_aspect_ratio_overrides_height_of_full_inset
+  absolute_aspect_ratio_fill_height
+  absolute_aspect_ratio_fill_height_from_inset
+  absolute_aspect_ratio_fill_max_height
+  absolute_aspect_ratio_fill_max_width
+  absolute_aspect_ratio_fill_min_height
+  absolute_aspect_ratio_fill_min_width
+  absolute_aspect_ratio_fill_width
+  absolute_aspect_ratio_fill_width_from_inset
+  absolute_aspect_ratio_height_overrides_inset
+  absolute_aspect_ratio_width_overrides_inset
+)
+
 YGBorderTests=(
   border_no_size
   border_container_match_child
   border_flex_child
   border_stretch_child
   border_center_child
+
+  # New from Taffy
+  border_no_child
 )
 
 YGDimensionTests=(
@@ -142,6 +218,10 @@ YGDisplayTests=(
   display_none_with_child
   display_none_with_position
   display_none_with_position_absolute
+  display_none_absolute_child
+
+  # New from Taffy
+  display_none_only_node
 )
 
 YGFlexDirectionTests=(
@@ -151,6 +231,7 @@ YGFlexDirectionTests=(
   flex_direction_row
   flex_direction_column_reverse
   flex_direction_row_reverse
+  flex_direction_column_reverse_no_height
 )
 
 YGFlexTests=(
@@ -164,6 +245,35 @@ YGFlexTests=(
   flex_basis_overrides_main_size
   flex_grow_shrink_at_most
   flex_grow_less_than_factor_one
+  single_flex_child_after_absolute_child
+  flex_basis_zero_undefined_main_size
+  only_shrinkable_item_with_flex_basis_zero
+
+  # New from Taffy
+  flex_basis_and_main_dimen_set_when_flexing
+  flex_basis_larger_than_content_column
+  flex_basis_larger_than_content_row
+  flex_basis_slightly_smaller_then_content_with_flex_grow_large_size
+  flex_basis_smaller_than_content_column
+  flex_basis_smaller_than_content_row
+  flex_basis_smaller_than_main_dimen_column
+  flex_basis_smaller_than_main_dimen_row
+  flex_basis_smaller_then_content_with_flex_grow_large_size
+  flex_basis_smaller_then_content_with_flex_grow_small_size
+  flex_basis_smaller_then_content_with_flex_grow_unconstraint_size
+  flex_basis_smaller_then_content_with_flex_grow_very_large_size
+  flex_basis_unconstraint_column
+  flex_basis_unconstraint_row
+  flex_basis_zero_undefined_main_size_hidden
+  flex_column_relative_all_sides
+  flex_grow_flex_basis_percent_min_max
+  flex_row_relative_all_sides
+  flex_shrink_by_outer_margin_with_max_size
+  width_smaller_then_content_with_flex_grow_large_size
+  width_smaller_then_content_with_flex_grow_small_size
+  width_smaller_then_content_with_flex_grow_unconstraint_size
+  width_smaller_then_content_with_flex_grow_very_large_size
+  relative_position_should_not_nudge_siblings
 )
 
 YGFlexWrapTests=(
@@ -187,6 +297,10 @@ YGFlexWrapTests=(
   wrapped_column_max_height_flex
   wrap_nodes_with_content_sizing_overflowing_margin
   wrap_nodes_with_content_sizing_margin_cross
+
+  # New from Taffy
+  wrap_reverse_column
+  wrap_reverse_row
 )
 
 
@@ -214,6 +328,11 @@ YGGapTests=(
   row_gap_column_child_margins
   row_gap_row_wrap_child_margins
   row_gap_determines_parent_height
+
+  # New from Taffy
+  column_gap_flexible_undefined_parent
+  column_gap_inflexible_undefined_parent
+  column_gap_row_gap_wrapping
 )
 
 YGJustifyContentTests=(
@@ -235,6 +354,11 @@ YGJustifyContentTests=(
   justify_content_row_space_evenly
   justify_content_min_width_with_padding_child_width_greater_than_parent
   justify_content_min_width_with_padding_child_width_lower_than_parent
+
+  # New from Taffy
+  justify_content_column_max_height_and_margin
+  justify_content_column_min_height_and_margin_bottom
+  justify_content_column_min_height_and_margin_top
 )
 
 YGMarginTests=(
@@ -275,11 +399,15 @@ YGMarginTests=(
 
 YGMinMaxDimensionTests=(
   max_width
+  min_width_larger_than_width
+  min_height_larger_than_height
   max_height
+  min_height_with_nested_fixed_height
   min_height
   min_width
   justify_content_min_max
   align_items_min_max
+  align_items_center_min_max_with_padding
   justify_content_overflow_min_max
   flex_grow_to_min
   flex_grow_in_at_most_container
@@ -300,6 +428,18 @@ YGMinMaxDimensionTests=(
   min_height_overrides_height
   max_height_overrides_height
   min_max_percent_no_width_height
+  min_max_percent_different_width_height
+  undefined_height_with_min_max
+  undefined_width_with_min_max
+  undefined_width_with_min_max_row
+
+  # New from Taffy
+  min_height_overrides_height_on_root
+  min_height_overrides_max_height
+  min_width_overrides_max_width
+  min_width_overrides_width_on_root
+  max_height_overrides_height_on_root
+  max_width_overrides_width_on_root
 )
 
 YGPaddingTests=(
@@ -309,6 +449,10 @@ YGPaddingTests=(
   padding_stretch_child
   padding_center_child
   child_with_padding_align_end
+
+  # New from Taffy
+  padding_align_end_child
+  padding_no_child
 )
 
 YGPercentageTests=(
@@ -324,6 +468,7 @@ YGPercentageTests=(
   percentage_flex_basis_cross_max_width
   percentage_flex_basis_main_min_width
   percentage_flex_basis_cross_min_width
+  percentage_main_max_height
   percentage_multiple_nested_with_padding_margin_and_percentage_values
   percentage_margin_should_calculate_based_only_on_width
   percentage_padding_should_calculate_based_only_on_width
@@ -332,6 +477,18 @@ YGPercentageTests=(
   percent_within_flex_grow
   percentage_container_in_wrapping_container
   percent_absolute_position
+  percentage_different_width_height
+  percentage_different_width_height_column
+
+  # New from Taffy
+  percentage_different_width_height
+  percentage_different_width_height_column
+  percentage_main_max_height
+  percentage_moderate_complexity
+  percentage_moderate_complexity2
+  percentage_size_based_on_parent_inner_size
+  percentage_size_of_flex_basis
+  percentage_sizes_should_not_prevent_flex_shrinking
 )
 
 YGRoundingTests=(
@@ -348,16 +505,27 @@ YGRoundingTests=(
   rounding_inner_node_controversy_horizontal
   rounding_inner_node_controversy_vertical
   rounding_inner_node_controversy_combined
+
+  # New from Taffy
+  rounding_fractial_input_5
+  rounding_fractial_input_6
+  rounding_fractial_input_7
 )
 
 YGSizeOverflowTests=(
   nested_overflowing_child
   nested_overflowing_child_in_constraint_parent
   parent_wrap_child_size_overflowing_parent
+
+  # New from Taffy
+  overflow_cross_axis
+  overflow_main_axis
+  overflow_main_axis_shrink_hidden
+  overflow_main_axis_shrink_visible
 )
 
 #Every known test
-ALL_YOGA_TESTS="${YGAlignContentTests[@]} ${YGBorderTests[@]} ${YGMarginTests[@]} ${YGPaddingTests[@]} ${YGMinMaxDimensionTests[@]} ${YGAlignItemsTests[@]} ${YGDimensionTests[@]} ${YGJustifyContentTests[@]} ${YGFlexDirectionTests[@]} ${YGAlignSelfTests[@]} ${YGRoundingTests[@]} ${YGPercentageTests[@]} ${YGGapTests[@]} ${YGDisplayTests[@]} ${YGFlexTests[@]} ${YGAndroidNewsFeedTests[@]} ${YGFlexWrapTests[@]} ${YGSizeOverflowTests[@]} ${YGAbsolutePositionTests[@]}"
+ALL_YOGA_TESTS="${YGAlignContentTests[@]} ${YGBorderTests[@]} ${YGMarginTests[@]} ${YGPaddingTests[@]} ${YGMinMaxDimensionTests[@]} ${YGAlignItemsTests[@]} ${YGDimensionTests[@]} ${YGJustifyContentTests[@]} ${YGFlexDirectionTests[@]} ${YGAlignSelfTests[@]} ${YGAspectRatioTests[@]} ${YGRoundingTests[@]} ${YGPercentageTests[@]} ${YGGapTests[@]} ${YGDisplayTests[@]} ${YGFlexTests[@]} ${YGAndroidNewsFeedTests[@]} ${YGFlexWrapTests[@]} ${YGSizeOverflowTests[@]} ${YGAbsolutePositionTests[@]}"
 ALL_YOGA_TESTS_WITH_SPACE=" $ALL_YOGA_TESTS "
 
 # for str in $EVERY_KNOWN_TEST; do
@@ -373,6 +541,6 @@ ALL_TAFFY_TESTS=`ls yoga_test_fixtures | sd '.html' ''`
 for test_name in $ALL_TAFFY_TESTS; do
   if [[ ! $ALL_YOGA_TESTS_WITH_SPACE =~ " ${test_name} " ]]; then
       # whatever you want to do when array doesn't contain value
-      echo "$test_name.html"
+      echo "$test_name" #.html"
   fi
 done

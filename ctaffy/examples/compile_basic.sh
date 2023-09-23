addbcb4dda0a3c772a9c76e186b04439f51c5d0f
Nico Burns <nico@nicoburns.com>
2023-09-23 22:06:12 +0100

    Remove bindgen from compile_basic.sh

--- ctaffy/examples/compile_basic.sh
+++ ctaffy/examples/compile_basic.sh
@@ -2,6 +2,5 @@
 
 set -ex
 
-(cd .. && cbindgen --crate ctaffy --output include/taffy.h)
 cargo build --manifest-path ../Cargo.toml
 gcc -O3 -DDEBUG -o basic basic.c -std=c99 -Wall -I../include -L../../target/debug -lctaffy

186ee5170c2efab414bf996adeb87e6209a398d7
Dean Srebnik <49134864+load1n9@users.noreply.github.com>
2024-01-24 14:39:02 -0500

    feat: deno bindings

---
+++ bindings/wasm/wasm_test_deno.ts
@@ -0,0 +1,11 @@
+import { instantiate, StyleUnit } from "./lib/taffy_layout.generated.js"; 
+
+const {Node, TaffyTree} = await instantiate();
+
+const tree = new TaffyTree();
+
+const node = new Node(tree);
+
+node.setWidth(10, StyleUnit)
+
+console.log(node.computeLayout(0).width);
